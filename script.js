e9d811a8622066945c420347b7be55f4465275cd
Deep Ghotane <ghotane.deep@gmail.com>
2025-06-26 17:27:41 +1000

    Updated enter-key press

--- script.js
+++ script.js
@@ -48,3 +48,9 @@ async function checkWeather(city) {
 searchBtn.addEventListener("click", () => {
   checkWeather(searchBox.value);
 });
+
+searchBox.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    checkWeather(searchBox.value);
+  }
+});
