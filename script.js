94d0f18492a24440330951ab61b918d6bf28b8ca
Deep Ghotane <ghotane.deep@gmail.com>
2025-06-26 17:12:36 +1000

    Updated

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Weather API key and the API url
 const apiKey = "25f4dc9385b919a5baed9c699bb67523 ";
 const apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
@@ -6,23 +7,27 @@ const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
+// Function to fetch the API
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
+  //   Condition to display error message when city name is invalid
   if (response.status == 404) {
     document.querySelector(".error").style.display = "block";
     document.querySelector(".weather").style.display = "none";
   }
   let data = await response.json();
 
-  console.log(data);
+  //   console.log(data);
 
+  // Populating the information on the page using query selector
   document.getElementById("city").innerHTML =
     data.name + ", " + data.sys.country;
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
   document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr";
 
+  //   Changing images based on the weather condition the city
   if (data.weather[0].main == "Clouds") {
     weatherIcon.src = "images/clouds.png";
   } else if (data.weather[0].main == "Clear") {
@@ -37,6 +42,8 @@ async function checkWeather(city) {
 
   document.querySelector(".weather").style.display = "block";
 }
+
+// Event listener to perform search and populate the data when search button is pressed
 
 searchBtn.addEventListener("click", () => {
   checkWeather(searchBox.value);
